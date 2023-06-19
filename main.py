54b6cd78a1aad382c695b103c4af1a243d42b0d2
duong-vo <vodh2@miamioh.edu>
2023-06-19 17:35:39 +0700

    add checking request, receive status pending instead of getting data

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ TOKEN = os.getenv("CSRF_TOKEN")
 SESSION = os.getenv("LEETCODE_SESSION")
 BASE_URL = 'https://leetcode.com/problems/'
 
+
+
 COOKIE = f"csrftoken={TOKEN}; LEETCODE_SESSION={SESSION}"
+
 print(COOKIE)
 HEADERS = {"Referer": BASE_URL + 'two-sum/',
            "Content-type": "application/json",
@@ -17,10 +20,33 @@ HEADERS = {"Referer": BASE_URL + 'two-sum/',
            "Cookie": COOKIE
 }
 
+
 data = {
     "lang": "python3",
     "question_id": "1",
     "typed_code": "class Solution:\n    def twoSum(self, nums: List[int], target: int) -> List[int]:\n        # O(n) idea: check if the difference exist\n        # within the nums using a map\n        map = dict()\n        for i in range(len(nums)):\n            # check if the difference is already\n            # in the map\n            diff = target - nums[i]\n            if diff in map:\n                return [map[diff], i]\n            # if we cannot find,\n            # map the current number to its index"
 }
+
 res = requests.post(BASE_URL + 'two-sum/submit/', headers=HEADERS, json=data)
-print(res.json())
+res_json = res.json()
+submission_id = res_json["submission_id"]
+
+print(submission_id)
+
+CHECK_TOKEN = os.getenv("CHECK_CSRF_TOKEN")
+CHECK_SESSION = os.getenv("CHECK_LEETCODE_SESSION")
+
+
+CHECK_COOKIE = f"csrftoken={CHECK_TOKEN}; LEETCODE_SESSION={CHECK_SESSION}"
+
+CHECK_HEADERS = {"Referer": BASE_URL + f"two-sum/submissions/{submission_id}/",
+                 "Content-type": "application/json",
+                 "X-Requested-With": "XMLHttpRequest",
+                 "COOKIE": CHECK_COOKIE
+                 }
+CHECK_URL = f"https://leetcode.com/submissions/detail/{submission_id}/check/"
+print(CHECK_URL)
+check_res = requests.get(CHECK_URL, headers=CHECK_HEADERS)
+print(check_res.status_code)
+print(check_res.json())
+
