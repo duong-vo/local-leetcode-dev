40e4c2f4b13300ade97d938932d1bd7966836658
duong-vo <vodh2@miamioh.edu>
2023-06-19 16:53:10 +0700

    add basic structure for post request for submission

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 
 TOKEN = os.getenv("CSRF_TOKEN")
 SESSION = os.getenv("LEETCODE_SESSION")
-BASE_URL = 'https://leetcode.com/problems'
+BASE_URL = 'https://leetcode.com/problems/'
 
-print(SESSION)
+COOKIE = f"csrftoken={TOKEN}; LEETCODE_SESSION={SESSION}"
+print(COOKIE)
+HEADERS = {"Referer": BASE_URL + 'two-sum/',
+           "Content-type": "application/json",
+           "X-Requested-With": "XMLHttpRequest",
+           "X-CSRFToken": TOKEN,
+           "Cookie": COOKIE
+}
+
+data = {
+    "lang": "python3",
+    "question_id": "1",
+    "typed_code": "class Solution:\n    def twoSum(self, nums: List[int], target: int) -> List[int]:\n        # O(n) idea: check if the difference exist\n        # within the nums using a map\n        map = dict()\n        for i in range(len(nums)):\n            # check if the difference is already\n            # in the map\n            diff = target - nums[i]\n            if diff in map:\n                return [map[diff], i]\n            # if we cannot find,\n            # map the current number to its index"
+}
+res = requests.post(BASE_URL + 'two-sum/submit/', headers=HEADERS, json=data)
+print(res.json())
